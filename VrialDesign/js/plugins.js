d834d6afd2ae3fe55e449189d4d94fa9548a5d19
ahmedShokry <amody311@gmail.com>
2018-09-24 14:37:33 -0700

    VrialDesign

---
+++ VrialDesign/js/plugins.js
@@ -0,0 +1,14 @@
+/*global console, $*/
+$(function () {
+    
+    'use strict';
+    $('.anzl').click(function () {
+       
+        $('html, body').animate({
+            
+            scrollTop: '815'
+        }, 1000);
+        
+        
+    });
+});
